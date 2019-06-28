dd9303dbf84cac71f8967a4038468bff5ec4fcd1
PixelZerg <pixelzerg@gmail.com>
2019-06-29 00:31:51 +0100

    animation work

--- script.js
+++ script.js
@@ -10,11 +10,87 @@ function get_sentence(){
     return get_random(w_verb) + " in the " + get_random(w_bodypart) + " with a wet " + get_random(w_marine);
 }
 
-function anim(id, before, after){
+// // $({perc: 0}).animate({perc: 50}, {
+// //     duration: 300,
+// //     easing: "swing",
+// //     step: function(now) {
+// //       $('.word ul').css({
+// //         transform: 'translateY(-' + now + '%)'
+// //       });
+// //     }
+// //   });
+
+// function anim(id, before, after){
+//     // init the ul
+//     word_span = $('.word#'+id);
+//     word_span.empty();
+//     word_span.append("<ul> <li>"+before+"</li><li>"+after+"</li></ul>");
+
+//     w0 = $(word_span.find('li')[0]).width();
+//     w1 = $(word_span.find('li')[1]).width();
+
+//     $({t: 0}).animate({t: 100}, {
+//         duration: 300,
+//         easing: "swing",
+//         step: function(t) {
+//             word_span.find('ul').css({
+//                 transform: 'translateY(-' + t/2 + '%)',
+//                 width: t + 'px'
+//             });
+//         },
+//         complete: function(){
+//             word_span.empty();
+//             word_span.append(after);
+//         }
+//     });
+// }
+
+function init(id, word){
     // init the ul
     word_span = $('.word#'+id);
     word_span.empty();
-    word_span.append("<ul> <li>"+before+"</li><li>"+after+"</li></ul>");
+    word_span.append("<ul> <li><span>"+word+"</span></li></ul>");
+    w0 = $(word_span.find('li span')[0]).width();
+
+    word_span.find('ul').css("width", w0+"px");
+}
+
+function anim(id, word){
+    // init the ul
+    word_ul = $('.word#'+id+' ul');
+    word_ul.append("<li><span>"+word+"</span></li>");
+    anim_done = false;
+
+    $(word_ul).ready(function(){
+        children_spans = word_ul.find('li span');
+        w0 = $(children_spans[0]).innerWidth();
+    
+        word_ul.css("width", w0+"px");
+    
+        if(children_spans.length > 1){
+            w1 = $(children_spans[1]).width();
+            $({t: 0}).animate({t: 100}, {
+                duration: 300,
+                easing: "swing",
+                step: function(t) {
+                    if(anim_done) return;
+                    
+                    word_ul.css({
+                        transform: 'translateY(-' + t/2 + '%)',
+                        width: w0 + (w1-w0)*(t/100) + 'px'
+                    });
+                },
+                complete: function(){
+                    if(anim_done) return;
+                    
+                    $(word_ul.find('li')[0]).remove();
+                    word_ul.css({transform: 'translateY(0%)'});
+                    console.log("wow");
+                    anim_done = true;
+                }
+            });
+        }
+    })
 }
 
 $(document).ready(function() {
@@ -26,8 +102,16 @@ $(document).ready(function() {
     $('head').append('<style>span{height: '+ h +'px}</style>');
     loading.remove();
 
-    sentence_div.append('<span class="word" id="verb"> <ul> <li>Poke</li><li>Tickle</li></ul> </span> <span> in the </span> <span class="word" id="bodypart"> <ul> <li>eye</li><li>nose</li></ul> </span> <span> with a wet </span> <span class="word" id="marine"> <ul> <li>cod</li><li>starfish</li></ul> </span>');
-    anim('verb', "before", "after");
+    sentence_div.append('<span class="word" id="verb"> <ul></ul> </span> <span> in the </span> <span class="word" id="bodypart"> <ul></ul> </span> <span> with a wet </span> <span class="word" id="marine"> <ul></ul> </span>');
+    // anim('verb', "before", "after");
+    anim('verb', 'Poke');
+    anim('bodypart', 'eye');
+    anim('marine', 'cod');
+
+    anim('verb', 'Eat');
+
+
+
 });
 
 
